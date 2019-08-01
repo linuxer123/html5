83f3ffdf01aa14202c52ff9005c9c01750af9195
john <qq@qq.com>
2019-08-01 16:09:03 +0800

    first commit

---
+++ js/changeFont.js
@@ -0,0 +1,16 @@
+$(function(){
+	$(window).resize(infinite);
+	function infinite() {
+		var htmlWidth = $('html').width();
+		if (htmlWidth >= 1080) {
+			$("html").css ({
+				"font-size" : "42px"
+			});
+		} else {
+			console.log(htmlWidth);
+			$("html").css ({
+				"font-size" : 42/1080 * htmlWidth + "px"
+			});
+		}
+	} infinite();
+})
